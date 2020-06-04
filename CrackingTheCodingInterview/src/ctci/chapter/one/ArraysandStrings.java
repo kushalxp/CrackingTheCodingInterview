4c573a531af4929e906724dd07daf19f7cab6bc6
Kushal <Kushal@192.168.0.107>
2020-06-04 23:52:26 +0530

    Solved 1.3 URLify

--- CrackingTheCodingInterview/src/ctci/chapter/one/ArraysandStrings.java
+++ CrackingTheCodingInterview/src/ctci/chapter/one/ArraysandStrings.java
@@ -1,7 +1,5 @@
 package ctci.chapter.one;
 
-import java.util.HashSet;
-
 public class ArraysandStrings {
 
 	/*
@@ -48,12 +46,54 @@ public class ArraysandStrings {
 
 	}
 
+	/*
+	 * 1.3 URLify: Write a method to replace all spaces in a string with '%20: You
+	 * may assume that the string has sufficient space at the end to hold the
+	 * additional characters, and that you are given the "true" length of the
+	 * string. (Note: If implementing in Java, please use a character array so that
+	 * you can perform this operation in place.) EXAMPLE Input: "Mr John Smith", 13
+	 * Output: "Mr%20John%20Smith"
+	 */
+	public void URLify(char[] str, int length) {
+
+		int countspace = 0;
+		int index;
+
+		for (int i = 0; i <= length - 1; i++) {
+			if (str[i] == ' ') {
+				countspace++;
+			}
+		}
+		index = length + countspace * 2;
+
+		for (int i = length - 1; i >= 0; i--) {
+			if (str[i] == ' ') {
+				str[index - 1] = '0';
+				str[index - 2] = '2';
+				str[index - 3] = '%';
+
+				index = index - 3;
+			} else {
+				str[index - 1] = str[i];
+				index--;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 
 		ArraysandStrings obj = new ArraysandStrings();
-		String stringOne = "aab";
-		String stringTwo = "abb";
-		System.out.println(obj.checkPermutation(stringOne, stringTwo));
+		/*
+		 * 1.2 String stringOne = "aab"; String stringTwo = "abb";
+		 * System.out.println(obj.checkPermutation(stringOne, stringTwo));
+		 */
+		
+		String str = "Mr john smith    ";
+		System.out.println("The given string is: " + str);
+		char[] arr = str.toCharArray();
+		obj.URLify(arr, 13);
+		String string = String.valueOf(arr);
+		System.out.print("The urlified string is: "+ string);
 	}
 
 }
