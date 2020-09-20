e5c64561b179c7da28289ffae5c2fb252adf5079
Kushal <bhatia.kushal@gmail.com>
2020-09-20 23:21:00 +0530

    Solved 2.3 Delete Middle Node

--- CrackingTheCodingInterview/src/ctci/chapter/two/LinkedLists.java
+++ CrackingTheCodingInterview/src/ctci/chapter/two/LinkedLists.java
@@ -46,22 +46,42 @@ public class LinkedLists {
 	}
 
 	/*
-	 * Return Kth to Last: Implement an algorithm to find the kth to last element of
-	 * a singly linked list.
+	 * 2.2 Return Kth to Last: Implement an algorithm to find the kth to last
+	 * element of a singly linked list.
 	 */
 	public ListNode returnKthToLastElement(ListNode n, int k) {
+		ListNode fast = n;
+		ListNode slow = n;
+
 		int index = 0;
 
-		while (n != null) {
-
-			if (index == k) {
-				return n;
-			}
+		while (index != k) {
 			index++;
-			n = n.next;
+			fast = fast.next;
 		}
 
-		return null;
+		while (fast != null) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+
+		return slow;
+	}
+
+	/*
+	 * 2.3 Delete Middle Node: Implement an algorithm to delete a node in the middle
+	 * (i.e., any node but the first and last node, not necessarily the exact
+	 * middle) of a singly linked list, given only access to that node. EXAMPLE
+	 * Input: the node c from the linked list a - >b- >c - >d - >e- >f Result:
+	 * nothing is returned, but the new linked list looks like a - >b- >d - >e- >f
+	 */
+
+	public void deleteMiddleNode(ListNode n) {
+		if(n != null && n.next != null) {
+			ListNode next = n.next;
+			n.val = next.val;
+			n.next = next.next;
+		}
 	}
 
 	public static void main(String[] args) {
@@ -70,10 +90,10 @@ public class LinkedLists {
 
 		ListNode head = new ListNode(1);
 		ListNode seco = new ListNode(2);
-		ListNode thir = new ListNode(1);
+		ListNode thir = new ListNode(3);
 		ListNode four = new ListNode(4);
 		ListNode five = new ListNode(5);
-		ListNode sixx = new ListNode(1);
+		ListNode sixx = new ListNode(6);
 
 		head.next = seco;
 		seco.next = thir;
@@ -88,9 +108,14 @@ public class LinkedLists {
 //		obj.printList(ans);
 
 //		2.2
+//		obj.printList(head);
+//		ListNode ans = obj.returnKthToLastElement(head, 4);
+//		obj.printList(ans);
+
+//		2.3
 		obj.printList(head);
-		ListNode ans = obj.returnKthToLastElement(head, 4);
-		obj.printList(ans);
+		obj.deleteMiddleNode(thir);
+		obj.printList(head);
 
 	}
 
